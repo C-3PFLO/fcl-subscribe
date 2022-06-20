4f2f479e4e51763acbf5a8539962c30212993701
C-3PFLO <c.3pflo.1@gmail.com>
2022-06-20 00:12:17 -0400

    initial implementation - unit tested, but requires real testing

--- README.md
+++ README.md
@@ -1,17 +1,41 @@
 # fcl-subscribe
 
-Mixin for `@onflow/fcl` to enable subscribing to blockchain events
+Subscribe to Flow blockchain events.
+
+## Motivation
+
+The `fcl.send()` function ([link](https://docs.onflow.org/fcl/reference/api/#send)) "*sends arbitrary scripts, transactions, and requests to Flow*".  Clients can use `fcl.send()` with the `fcl.getEventsAtBlockHeightRange()` function ([link](https://docs.onflow.org/fcl/reference/api/#geteventsatblockheightrange)) to get "*all instances of a particular event (by name) within a height range*".
+
+However, `fcl.getEventsAtBlockHeightRange()` is limited to a single block range, "*the block range provided must be 250 blocks or lower per request.*" and `fcl.send()` only sends the request once.  There is no `@onflow/fcl` method to subscribe to all events of a certain type across a larger block range, including as new blocks are created.
+
+This module implements `subscribe()` which allows clients to crawl the blockchain, making the same `fcl.getEventsAtBlockHeightRange()` call across many block ranges (250 blocks at a time).  It also supports live polling as new blocks are created.
+
+*NOTE: while this module was designed for `fcl.getEventsAtBlockHeightRange()`, it can be used for any request that crawls the blocks.*
+
+## Usage
 
 ```js
 import * as fcl from '@onflow/fcl';
 import * as subscribe from 'fcl-subscribe';
 
-// mixin polling
-Object.assign(fcl, subscribe);
-
 // use standard fcl methods
-fcl.config(/* options */);
+fcl.config( /*arguments*/ );
 
-// use mixed-in method to subscribe
-fcl.subscribe(/* options */);
+// subscribe, caching the unsubscribe function
+const unsubscribe = subscribe({
+    block: fcl.block,
+    getQuery: function(context) {
+        return fcl.send([
+            fcl.getEventsAtBlockHeightRange(
+                eventName: 'MyEvent',
+                fromBlockHeight: context.fromBlockHeight,
+                toBlockHeight: context.toBlockHeight,
+            ),
+        ]);
+    },
+    onResponse: function(response) {
+        fcl.decode(response).then(console.log);
+    },
+    onError: console.error
+});
 ```
--- src/fcl-subscribe.js
+++ src/fcl-subscribe.js
@@ -1,9 +1,80 @@
+/* global setTimeout */
+
+const sleepTime = 1000;
+const range = 249;
 
 /**
-* Subscribe to a blockchain event
+* Subscribe an @onflow/fcl request across a range of block heights, including
+* newly created blocks.
+* @public
+* @param {Object} options
+* @param {Function} options.block fcl.block function
+* @param {Function} options.getQuery an fcl.send function, called with the current
+* block context to query the next range
+* @param {Function} options.onResponse response handler, which should call fcl.decode
+* to parse the response data
+* @param {Function} [options.onError] error handler
+* @param {Integer} [options.range = 249] block range to query per iteration
+* @param {Integer} [options.fromBlockHeight = <current>] starting block height
+* @param {Integer} [options.sleepTime = 1000] time to sleep between iterations
+* @param {Boolean} [options.abortOnError = false] abort subscription on error
+* @return {Function} unsubscribe
 */
-function subscribe() {}
+function subscribe(options) {
+    const context = {
+        range: options.range || range,
+        fromBlockHeight: options.fromBlockHeight,
+        sleepTime: options.sleepTime || sleepTime,
+        abortOnError: typeof options.abortOnError === 'undefined' ?
+            false : options.abortOnError,
+    };
 
-export {
-    subscribe,
-};
+    /**
+    * @private
+    */
+    function _loop() {
+        options.block(true)
+            .then((response) => {
+                context.height = response.height;
+                // if no fromBlockHeight was provided, start from context.height
+                // this will result in remaining = 0 which will skip and wait
+                // for new blocks to be created
+                context.fromBlockHeight = typeof context.fromBlockHeight !== 'undefined' ?
+                    context.fromBlockHeight : context.height;
+                context.remaining = context.height - context.fromBlockHeight;
+                context.toBlockHeight = context.fromBlockHeight +
+                    (context.remaining >= range ? range : context.remaining);
+                if (context.remaining > 0) {
+                    return options.getQuery(context)
+                        .then((response) => {
+                            options.onResponse(response);
+                        })
+                        .then(() => {
+                            context.fromBlockHeight = context.toBlockHeight + 1;
+                        });
+                } else {
+                    return Promise.resolve();
+                }
+            })
+            .catch((error) => {
+                if (options.onError) {
+                    options.onError(error);
+                }
+                if (context.abortOnError) {
+                    context.stop = true;
+                }
+            })
+            .then(() => {
+                if (!context.stop) {
+                    setTimeout(_loop, context.sleepTime);
+                }
+            });
+    }
+    _loop();
+
+    return function() {
+        context.stop = true;
+    };
+}
+
+export default subscribe;
